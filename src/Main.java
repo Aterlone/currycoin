9c33b7c67d706de6b1c338d9e69b23bd72e1eb67
Aterlone <taneahaines@gmail.com>
2024-11-21 16:31:58 +1300

    SHA-256

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import javax.crypto.*;
+import java.security.*;
+import java.util.*; 
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static String bytesToHex(byte[] arr) {
+		String hex = "";
+		for (byte i : arr) {
+			hex += String.format("%02X", i);
+		}
+		return hex;
+	}
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		MessageDigest mess = MessageDigest.getInstance("SHA-256");
+		mess.update("Hello".getBytes());
+
+		String md = bytesToHex(mess.digest());
+		System.out.println(md);
 	}
 }
