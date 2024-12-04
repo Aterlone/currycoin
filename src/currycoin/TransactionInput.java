a68d7d1b1c4b60bd67b4596f4e1fc0ca0f3a7da0
Max Cai <maxcai.paloalto@gmail.com>
2024-12-04 15:02:45 -0800

    some bits and bobs of TransactionInput

--- src/currycoin/TransactionInput.java
+++ src/currycoin/TransactionInput.java
@@ -1,7 +1,22 @@
 package currycoin;
 
 import currycoin.script.Script;
+import currycoin.script.ScriptException;
+import currycoin.script.ScriptStack;
 
 public record TransactionInput(Hash prevTransaction, int index, Script lockingScript, Script unlockingScript) {
-
+	/**
+	 * Checks whether the provided unlocking script can unlock the provided locking script.
+	 * Does not check if the provided locking script is correct the output it is trying to unlock.
+	 */
+	public boolean unlocks(Hash dataToSign) {
+		ScriptStack stack = new ScriptStack(dataToSign);
+		try {
+			lockingScript.execute(stack);
+			unlockingScript.execute(stack);
+			return true; // ran without failing
+		} catch (ScriptException e) {
+			return false;
+		}
+	}
 }
